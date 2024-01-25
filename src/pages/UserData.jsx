020c65db6e025e1333f00087831c5e366ac7135e
Rohitwagh812 <rohitwagh480@gmail.com>
2024-01-26 00:31:22 +0530

    first commit

--- src/pages/UserData.jsx
+++ src/pages/UserData.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function UserData() {
   return (
-    <div style={{padding:180, backgroundColor:'white', height:'100vh'}}> 
-        <h1 style={{fontWeight:"bold", fontSize:"5rem" }}> THIS PAGE COMING SOON  </h1>
-    </div>
+    <div className='services-page'> 
+
+    {/*  style={{padding:180, height:'100vh'}} */}
+      <h1> THIS PAGE COMING SOON  </h1>
+      {/*  style={{fontWeight:"bold", fontSize:"5rem" ,color:"#fff" }} */}
+  </div>
   )
 }
